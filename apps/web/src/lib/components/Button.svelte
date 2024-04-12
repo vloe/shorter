bede8a8b73ace9e82f47291e77419dea558d382b
vloe <vloe.dev@gmail.com>
2024-04-12 21:01:58 +0200

    darkmode (so over)

---
+++ apps/web/src/lib/components/Button.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { cn } from "$lib/utils/cn"
+	import type { HTMLButtonAttributes } from "svelte/elements"
+	import type { VariantProps } from "cva"
+	import { btnVariants } from "$lib/utils/variants"
+
+	interface $$Props extends HTMLButtonAttributes, VariantProps<typeof btnVariants> {}
+	export let variant: $$Props["variant"] = "primary"
+	export let size: $$Props["size"] = "default"
+</script>
+
+<button
+	{...$$restProps}
+	on:click|preventDefault
+	class={cn(btnVariants({ variant, size }), $$props.class)}
+>
+	<slot />
+</button>
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,1 +1,6 @@
-<p class="text-violet-500">god how do i ship this thing?</p>
+<script lang="ts">
+	import Theme from "$lib/components/Theme.svelte"
+</script>
+
+<p class="bg-background">god how do i ship this thing?</p>
+<Theme />
