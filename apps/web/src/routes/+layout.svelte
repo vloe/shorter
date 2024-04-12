673eb39cc7cbfaa09a235be90218df63de51ed07
vloe <vloe.dev@gmail.com>
2024-04-12 19:50:34 +0200

    rewrite in svelte 4

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,10 +1,5 @@
 <script>
 	import "../app.css"
-	import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query"
-
-	const queryClient = new QueryClient()
 </script>
 
-<QueryClientProvider client={queryClient}>
-	<slot />
-</QueryClientProvider>
+<slot />
