cb5d3047d4464f4c79e05b8ea9feffd5883074e6
vloe <vloe.dev@gmail.com>
2024-05-25 01:43:37 +0200

    rewrite

--- core/src/api/mod.rs
+++
@@ -1,9 +0,0 @@
-use axum::{routing::get, Router};
-
-mod shorten;
-
-pub fn mount() -> Router {
-    Router::new()
-        .route("/health", get(|| async { "OK" }))
-        .merge(shorten::mount())
-}
