094a3f4aa7c3ea9021814a22fa9e05051d140aa8
vloe <vloe.dev@gmail.com>
2024-05-25 23:09:45 +0200

    ui lib now works

--- packages/config/tailwind/tailwind.config.cjs
+++ packages/config/tailwind/tailwind.config.cjs
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: ["class"],
-	content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: [
+        "./src/**/*.{html,js,svelte,ts}",
+        '../../packages/ui/src/lib/**/*.{svelte,ts}'
+    ],
 	theme: {
 		container: {
 			center: true,
