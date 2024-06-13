5e0a6436dd4e5b4fcf9774c6a42ed6e794591f19
vloe <vloe.dev@gmail.com>
2024-06-13 12:46:39 +0200

    dude why doesnt cache work?

--- apps/landing/src/routes/+layout.svelte
+++ apps/landing/src/routes/+layout.svelte
@@ -1,9 +1,16 @@
 <script>
 	import "../app.css"
 	import { QueryClient, QueryClientProvider } from "@tanstack/svelte-query"
+	import { browser } from "$app/environment"
 	import Header from "$lib/components/Header.svelte"
 
-	const queryClient = new QueryClient()
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser,
+			},
+		},
+	})
 
 	let { children } = $props()
 </script>
