088fb070c29fafcf68cb474ac9aeae4f36b069a7
vloe <vloe.dev@gmail.com>
2024-06-14 17:46:23 +0200

    footer work

--- apps/landing/src/lib/components/Footer.svelte
+++ apps/landing/src/lib/components/Footer.svelte
@@ -1,3 +1,33 @@
+<script lang="ts">
+	import Github from "@sh/ui/src/lib/icons/Github.svelte"
+	import X from "@sh/ui/src/lib/icons/X.svelte"
+</script>
+
 <div class="container px-6">
-	<footer class="flex h-20 items-center justify-between">test</footer>
+	<footer class="flex h-28 items-center justify-between">
+		<div>
+			<h2 class="font-lumiera text-3xl">shorter</h2>
+			<p class="mt-1 text-sm text-muted-foreground">
+				© {new Date().getFullYear()} shorter, inc.
+			</p>
+		</div>
+		<div class="flex flex-row items-center space-x-4">
+			<a
+				href="https://github.com/vloe/shorter"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="transition-color text-primary duration-200 ease-in-out hover:text-primary/75"
+			>
+				<Github class="h-7 w-7" />
+			</a>
+			<a
+				href="https://x.com/vloedev"
+				target="_blank"
+				rel="noopener noreferrer"
+				class="transition-color text-primary duration-200 ease-in-out hover:text-primary/75"
+			>
+				<X class="h-6 w-6" />
+			</a>
+		</div>
+	</footer>
 </div>
