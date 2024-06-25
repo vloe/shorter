be843ba8e861d813c04d70d0be0dd537b7baa634
vloe <vloe.dev@gmail.com>
2024-06-25 03:05:11 +0200

    does this work?

--- apps/tlds/src/lib.rs
+++ apps/tlds/src/lib.rs
@@ -5,10 +5,27 @@ use worker::*;
 #[event(fetch)]
 async fn fetch(
     req: HttpRequest,
-    _env: Env,
+    env: Env,
     _ctx: Context,
 ) -> Result<axum::http::Response<axum::body::Body>> {
     console_error_panic_hook::set_once();
+
+    let kv = env.kv("tlds")?;
+
+    let res = reqwest::get("https://data.iana.org/TLD/tlds-alpha-by-domain.txt")
+        .await
+        .unwrap();
+
+    let text = res.text().await.unwrap();
+
+    for line in text.lines() {
+        if line.is_empty() || line.starts_with("#") || line.starts_with("XN--") {
+            continue;
+        }
+
+        let tld = line.to_lowercase();
+        kv.put(&tld, "test")?.execute().await?;
+    }
 
     let app = Router::new()
         .route("/", get(|| async { "sh-tlds" }))
