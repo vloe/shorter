efd3955373a86add021eaedac9da39b342ed99d6
vloe <vloe.dev@gmail.com>
2024-07-03 20:57:47 +0200

    clean up

--- apps/server/src/lib.rs
+++ apps/server/src/lib.rs
@@ -1,27 +1,16 @@
-use axum::{
-    body::Body,
-    extract::Request,
-    http::{header, HeaderValue, Method, Response},
-    middleware,
-    routing::get,
-    Router,
-};
+use axum::{http::HeaderValue, routing::get, Router};
 use sh_core::api::mount;
+use std::time::Duration;
 use tower_http::cors::CorsLayer;
 use tower_service::Service;
-use worker::{event, Context, Env, Error, HttpRequest};
-
-async fn cache(req: Request, next: middleware::Next) -> axum::response::Response {
-    let mut res = next.run(req).await;
-    res.headers_mut().insert(
-        header::CACHE_CONTROL,
-        "max-age=600".parse::<HeaderValue>().unwrap(),
-    );
-    res
-}
+use worker::*;
 
 #[event(fetch)]
-async fn fetch(req: HttpRequest, _env: Env, _ctx: Context) -> Result<Response<Body>, Error> {
+async fn fetch(
+    req: HttpRequest,
+    _env: Env,
+    _ctx: Context,
+) -> Result<axum::http::Response<axum::body::Body>> {
     console_error_panic_hook::set_once();
 
     let cors = CorsLayer::new()
@@ -30,14 +19,12 @@ async fn fetch(req: HttpRequest, _env: Env, _ctx: Context) -> Result<Response<Bo
             "http://localhost:3001".parse::<HeaderValue>().unwrap(),
             "https://shorter.dev".parse::<HeaderValue>().unwrap(),
         ])
-        .allow_headers([header::CONTENT_TYPE])
-        .allow_methods([Method::GET, Method::POST, Method::DELETE]);
+        .max_age(Duration::from_secs(3600));
 
     let app = Router::new()
         .route("/", get(|| async { "sh-server" }))
         .merge(mount())
         .layer(cors)
-        .layer(middleware::from_fn(cache))
         .call(req)
         .await?;
 
