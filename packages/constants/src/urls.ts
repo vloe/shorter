3eb90a2472734884f484918136ebcfce60f2ca1e
vloe <vloe.dev@gmail.com>
2024-07-11 23:44:07 +0200

    move prod to aws lambda

--- packages/constants/src/urls.ts
+++ packages/constants/src/urls.ts
@@ -1,2 +1,2 @@
-export const API_URL_DEV = "http://localhost:8787"
-export const API_URL_PROD = "https://api.shorter.dev"
+export const API_URL_DEV = "http://localhost:9000"
+export const API_URL_PROD = "https://cl6osbbujy5efjraspt52ykbzq0pabej.lambda-url.eu-west-2.on.aws" // fml
