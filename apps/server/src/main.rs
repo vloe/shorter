e7d93524687e67ac20c991e0d56b637f531d7bea
vloe <vloe.dev@gmail.com>
2024-07-26 00:11:43 +0200

    more clean up...

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -5,15 +5,18 @@ use axum::{
     BoxError, Router,
 };
 use hickory_resolver::TokioAsyncResolver;
-use lambda_http::{run, tracing, Error};
 use sh_core::api::{mount, Ctx};
 use std::time::Duration;
 use tower::{buffer::BufferLayer, limit::RateLimitLayer, ServiceBuilder};
 use tower_http::cors::CorsLayer;
 
 #[tokio::main]
-async fn main() -> Result<(), Error> {
-    tracing::init_default_subscriber();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    const MAX_AGE: u64 = 3600;
+    const RATE_LIMIT_PERIOD: u64 = 1;
+    const RATE_LIMIT_MAX_REQS: u64 = 10;
+    const RATE_LIMIT_BUFFER: usize = 1000;
+    const ADDR: &str = "127.0.0.1:9000";
 
     let cors = CorsLayer::new()
         .allow_origin([
@@ -22,9 +25,9 @@ async fn main() -> Result<(), Error> {
             "http://localhost:3002".parse::<HeaderValue>().unwrap(),
             "https://shorter.dev".parse::<HeaderValue>().unwrap(),
         ])
-        .allow_methods([Method::GET, Method::POST])
-        .allow_headers([header::CONTENT_TYPE])
-        .max_age(Duration::from_secs(3600));
+        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_headers([header::CONTENT_TYPE, header::AUTHORIZATION])
+        .max_age(Duration::from_secs(MAX_AGE));
 
     let rate_limit = ServiceBuilder::new()
         .layer(HandleErrorLayer::new(|err: BoxError| async move {
@@ -33,15 +36,13 @@ async fn main() -> Result<(), Error> {
                 format!("internal error: {}", err),
             )
         }))
-        .layer(BufferLayer::new(1000))
-        .layer(RateLimitLayer::new(10, Duration::from_secs(1)));
+        .layer(BufferLayer::new(RATE_LIMIT_BUFFER))
+        .layer(RateLimitLayer::new(
+            RATE_LIMIT_MAX_REQS,
+            Duration::from_secs(RATE_LIMIT_PERIOD),
+        ));
 
-    let resolver = {
-        let (cfg, opts) = hickory_resolver::system_conf::read_system_conf()?;
-        TokioAsyncResolver::tokio(cfg, opts).into()
-    };
-
-    let ctx = Ctx { resolver };
+    let ctx = get_ctx()?;
 
     let app = Router::new()
         .route("/", get(|| async { "sh-server" }))
@@ -49,6 +50,41 @@ async fn main() -> Result<(), Error> {
         .layer(cors)
         .layer(rate_limit);
 
-    println!("\nlistening on: http://localhost:9000\n");
-    run(app).await
+    #[cfg(feature = "lambda")]
+    run_prod(app).await?;
+
+    #[cfg(not(feature = "lambda"))]
+    run_dev(app, ADDR).await?;
+
+    Ok(())
+}
+
+fn get_ctx() -> Result<Ctx, Box<dyn std::error::Error>> {
+    let resolver = {
+        let (cfg, opts) = hickory_resolver::system_conf::read_system_conf()?;
+        TokioAsyncResolver::tokio(cfg, opts).into()
+    };
+
+    let ctx = Ctx { resolver };
+
+    Ok(ctx)
+}
+
+#[cfg(feature = "lambda")]
+async fn run_prod(app: Router) -> Result<(), Box<dyn std::error::Error>> {
+    use lambda_http::{run, tracing, Error};
+
+    tracing::init_default_subscriber();
+    run(app).await?;
+
+    Ok(())
+}
+
+#[cfg(not(feature = "lambda"))]
+async fn run_dev(app: Router, addr: &str) -> Result<(), Box<dyn std::error::Error>> {
+    println!("\nlistening on http://{}\n", addr);
+    let listener = tokio::net::TcpListener::bind(&addr).await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
