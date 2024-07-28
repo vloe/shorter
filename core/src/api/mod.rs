42c5b7e423b0bdc19f668d29b2fd6818e562c21d
vloe <vloe.dev@gmail.com>
2024-07-28 21:28:51 +0200

    please just work

--- core/src/api/mod.rs
+++ core/src/api/mod.rs
@@ -1,6 +1,6 @@
 use axum::{routing::get, Router};
-use bitvec::prelude::*;
 use hickory_resolver::TokioAsyncResolver;
+use memmap2::Mmap;
 use std::sync::Arc;
 
 mod shorten;
@@ -8,7 +8,7 @@ mod shorten;
 #[derive(Clone)]
 pub struct Ctx {
     pub resolver: Arc<TokioAsyncResolver>,
-    pub domains: &'static BitSlice<u8, Msb0>,
+    pub domains: Arc<Mmap>,
 }
 
 pub fn mount(ctx: Ctx) -> Router {
--- scripts/src/bin/gen_domains.rs
+++ scripts/src/bin/gen_domains.rs
@@ -1,34 +1,35 @@
 use dotenv::dotenv;
 use flate2::read::GzDecoder;
 use memmap2::MmapOptions;
+use reqwest::Client;
 use serde_json::{json, Value};
 use sh_crypto::hash::Hash;
-use sh_domain::{domain_available::BITMAP_SIZE, tlds::TLDS};
+use sh_domain::{
+    domain::{DOMAINS_BIT_SIZE, DOMAINS_BYTE_SIZE},
+    tlds::TLDS,
+};
 use std::{env, error::Error, fs::OpenOptions, io::prelude::*, path::Path};
 use tokio::{
     fs::{self, File},
     io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
 };
 
+const CZDS_API_URL: &str = "https://czds-api.icann.org/czds/downloads/links";
+const CZDS_API_AUTH_URL: &str = "https://account-api.icann.org/api/authenticate";
+const ZONE_DIR: &str = "../apps/server/tmp/";
+const DOMAINS_FILE: &str = "../apps/server/data/domains.bin";
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     dotenv().ok();
 
-    const CZDS_API_URL: &str = "https://czds-api.icann.org/czds/downloads/links";
-    const CZDS_API_AUTH_URL: &str = "https://account-api.icann.org/api/authenticate";
-    const ZONE_DIR: &str = "../apps/server/src/tmp/";
-    const DOMAINS_FILE: &str = "../apps/server/data/domains.bin";
-
-    let client = reqwest::Client::builder()
-        .user_agent("curl/7.79.1")
-        .build()?;
-
-    let access_token = get_access_token(&client, CZDS_API_AUTH_URL).await?;
-    let zone_urls = get_zone_urls(&client, CZDS_API_URL, &access_token).await?;
+    let client = Client::builder().user_agent("curl/7.79.1").build()?;
+    let access_token = get_access_token(&client).await?;
+    let zone_urls = get_zone_urls(&client, &access_token).await?;
 
     let mut total_bits_used = 0;
     for (i, zone_url) in zone_urls.iter().enumerate() {
-        let (zone, file_path) = parse_zone_url(zone_url, ZONE_DIR).await?;
+        let (zone, file_path) = parse_zone_url(zone_url).await?;
 
         if TLDS.get(&zone.as_str()).is_none() {
             println!("invalid zone: {}", zone);
@@ -39,7 +40,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
             println!("failed to download zone: {}, err: {}", zone_url, e);
             continue;
         };
-        match bitmap_zone(&file_path, DOMAINS_FILE, BITMAP_SIZE).await {
+
+        match bitmap_zone(&file_path, DOMAINS_FILE).await {
             Ok(bits_used) => {
                 println!(
                     "{}/{}: {} ({} bits)",
@@ -58,9 +60,8 @@ async fn main() -> Result<(), Box<dyn Error>> {
     }
 
     println!(
-        "total bits: {}, bitmap bits: {}",
-        total_bits_used,
-        BITMAP_SIZE * 8
+        "total bits: {}, domains bits: {}",
+        total_bits_used, DOMAINS_BIT_SIZE
     );
 
     fs::remove_dir_all(ZONE_DIR).await?;
@@ -68,9 +69,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn get_access_token(client: &reqwest::Client, url: &str) -> Result<String, Box<dyn Error>> {
+async fn get_access_token(client: &Client) -> Result<String, Box<dyn Error>> {
     let res = client
-        .post(url)
+        .post(CZDS_API_AUTH_URL)
         .json(&json!({
             "username": env::var("ICANN_USERNAME").unwrap(),
             "password": env::var("ICANN_PASSWORD").unwrap(),
@@ -88,11 +89,10 @@ async fn get_access_token(client: &reqwest::Client, url: &str) -> Result<String,
 
 async fn get_zone_urls(
     client: &reqwest::Client,
-    url: &str,
     access_token: &str,
 ) -> Result<Vec<String>, Box<dyn Error>> {
     let res = client
-        .get(url)
+        .get(CZDS_API_URL)
         .header("Authorization", format!("Bearer {}", access_token))
         .send()
         .await?;
@@ -102,18 +102,15 @@ async fn get_zone_urls(
     Ok(zone_urls)
 }
 
-async fn parse_zone_url(
-    zone_url: &str,
-    zone_dir: &str,
-) -> Result<(String, String), Box<dyn Error>> {
+async fn parse_zone_url(zone_url: &str) -> Result<(String, String), Box<dyn Error>> {
     let data = zone_url.split('/').last().unwrap().replace(".zone", "");
     let zone = format!(".{}", data);
     let file_name = format!("{}.txt", data);
-    let file_path = format!("{}{}", zone_dir, file_name);
+    let file_path = format!("{}{}", ZONE_DIR, file_name);
 
     // create dir if it doesn't exist
-    if !Path::new(zone_dir).exists() {
-        fs::create_dir_all(zone_dir).await?;
+    if !Path::new(ZONE_DIR).exists() {
+        fs::create_dir_all(ZONE_DIR).await?;
     }
 
     Ok((zone, file_path))
@@ -147,7 +144,6 @@ async fn download_zone(
 async fn bitmap_zone(
     file_path: &str,
     domains_file: &str,
-    bitmap_size: usize,
 ) -> Result<i32, Box<dyn std::error::Error>> {
     let file = OpenOptions::new()
         .read(true)
@@ -155,7 +151,7 @@ async fn bitmap_zone(
         .create(true)
         .open(domains_file)?;
 
-    file.set_len(bitmap_size as u64)?;
+    file.set_len(DOMAINS_BYTE_SIZE as u64)?;
 
     let mut mmap = unsafe { MmapOptions::new().map_mut(&file)? };
 
@@ -175,7 +171,7 @@ async fn bitmap_zone(
         }
 
         if !domain.is_empty() {
-            let index = Hash::domain_to_index(domain, bitmap_size);
+            let index = Hash::domain_to_index(domain, DOMAINS_BIT_SIZE);
             mmap[index / 8] |= 1 << (7 - (index % 8));
             bits_used += 1;
         }
