ed767e0d4cceb907c67ebc06c7db855b9cb2e27b
vloe <vloe.dev@gmail.com>
2024-08-02 11:25:47 +0200

    think header is fine

--- apps/landing/src/lib/components/Header.svelte
+++ apps/landing/src/lib/components/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Logomark from "$lib/assets/svgs/white-logomark.svg"
+	import Lockup from "$lib/assets/svgs/white-lockup.svg"
 	import Arrow from "$lib/icons/Arrow.svelte"
 	import Menu from "$lib/icons/Menu.svelte"
 	import { Btn } from "$lib/ui/btn"
@@ -11,26 +12,40 @@
 	} from "$lib/ui/dropdown-menu"
 </script>
 
-<header class="flex h-16 flex-row items-center justify-between">
+<header class="flex h-[72px] flex-row items-center justify-between">
 	<a href="/">
-		<img class="h-7" src={Logomark} alt="logomark" />
+		<img class="flex h-7 lg:hidden" src={Logomark} alt="logomark" />
+		<img class="hidden h-7 lg:flex" src={Lockup} alt="lockup" />
 	</a>
 	<div class="hidden items-center gap-x-2 lg:flex">
 		<Btn class="group gap-x-[5px] duration-200 ease-in-out">
 			start now
 			<Arrow />
 		</Btn>
-		<Btn intent="secondary" class="duration-200 ease-in-out">login</Btn>
+		<Btn
+			intent="secondary"
+			href="https://github.com/vloe/shorter"
+			target="_blank"
+			rel="noopener noreferrer"
+			class="duration-200 ease-in-out"
+		>
+			star on github
+		</Btn>
 	</div>
 	<div class="flex lg:hidden">
 		<DropdownMenu>
-			<DropdownMenuTrigger asChild let:builder>
-				<Btn builders={[builder]} intent="ghost" size="icon"><Menu /></Btn>
+			<DropdownMenuTrigger>
+				<Menu class="size-7 text-white" />
 			</DropdownMenuTrigger>
 			<DropdownMenuContent class="w-48">
-				<DropdownMenuItem>test 1</DropdownMenuItem>
-				<DropdownMenuItem>test 2</DropdownMenuItem>
-				<DropdownMenuItem>test 3</DropdownMenuItem>
+				<DropdownMenuItem>start now</DropdownMenuItem>
+				<DropdownMenuItem
+					href="https://github.com/vloe/shorter"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					repo
+				</DropdownMenuItem>
 			</DropdownMenuContent>
 		</DropdownMenu>
 	</div>
