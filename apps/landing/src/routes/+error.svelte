fd6f4060b2a0754b60bd438e51c95ccbae05a341
vloe <vloe.dev@gmail.com>
2024-08-09 00:35:48 +0200

    added error page

---
+++ apps/landing/src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { page } from "$app/stores"
+</script>
+
+<div class="flex h-screen items-center justify-center">
+	<h1 class="font-medium">{$page.status}: {$page.error?.message}</h1>
+</div>
