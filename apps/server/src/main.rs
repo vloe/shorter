e03d630c5d9de6c22055b3bb687244380fd4df4a
vloe <vloe.dev@gmail.com>
2024-08-24 16:35:02 +0200

    try to publish container to aws lambda

--- apps/server/src/main.rs
+++ apps/server/src/main.rs
@@ -1,4 +1,4 @@
-use axum::routing::get;
+use axum::{routing::get, Router};
 use std::error::Error;
 
 mod utils;
@@ -7,10 +7,31 @@ const ADDR: &str = "127.0.0.1:9000";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let app = axum::Router::new()
+    let app = Router::new()
         .route("/", get(|| async { "sh-server(:" }))
         .route("/health", get(|| async { "ok" }));
 
+    #[cfg(feature = "lambda")]
+    run_lambda(app).await?;
+
+    #[cfg(not(feature = "lambda"))]
+    run_dev(app).await?;
+
+    Ok(())
+}
+
+#[cfg(feature = "lambda")]
+async fn run_lambda(app: Router) -> Result<(), Box<dyn Error>> {
+    use lambda_http::{run, tracing};
+
+    tracing::init_default_subscriber();
+    run(app).await;
+
+    Ok(())
+}
+
+#[cfg(not(feature = "lambda"))]
+async fn run_dev(app: Router) -> Result<(), Box<dyn Error>> {
     println!("\nlistening on http://{}\n", ADDR);
     let listener = tokio::net::TcpListener::bind(&ADDR).await?;
     axum::serve(listener, app)
