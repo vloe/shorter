f75e99b1bc1ed8d6d45b34a6baa0a656356d7b03
vloe <vloe.dev@gmail.com>
2024-08-27 03:18:02 +0200

    added fallback

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -4,7 +4,9 @@ import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: "404.html",
+		}),
 		paths: {
 			relative: false,
 		},
