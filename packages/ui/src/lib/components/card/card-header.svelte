d25c997468f7ca1c0681480c50078fa36a7cdb16
vloe <vloe.dev@gmail.com>
2024-08-27 03:11:09 +0200

    login/register page

---
+++ packages/ui/src/lib/components/card/card-header.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import type { Snippet } from "svelte"
+	import type { HTMLAttributes } from "svelte/elements"
+
+	import { cx } from "@sh/utils/src/cva.config"
+
+	type $Props = {
+		children: Snippet
+	} & HTMLAttributes<HTMLDivElement>
+
+	let { children, class: className, ...props }: $Props = $props()
+</script>
+
+<div class={cx("flex flex-col space-y-1.5 p-6", className)} {...props}>
+	{@render children()}
+</div>
---
+++ packages/ui/src/lib/components/card/card.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { Snippet } from "svelte"
+	import type { HTMLAttributes } from "svelte/elements"
+
+	import { cx } from "@sh/utils/src/cva.config"
+
+	type $Props = {
+		children: Snippet
+		logo: boolean
+	} & HTMLAttributes<HTMLDivElement>
+
+	let { children, class: className, ...props }: $Props = $props()
+</script>
+
+<div class={cx("rounded-xl border bg-card text-card-foreground shadow", className)} {...props}>
+	{@render children()}
+</div>
