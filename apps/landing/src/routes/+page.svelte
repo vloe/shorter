f1a26a6b3d632c1ab1f8b69369128aa6f85ac106
vloe <vloe.dev@gmail.com>
2024-08-30 04:44:50 +0200

    some landing work

--- apps/landing/src/routes/+page.svelte
+++ apps/landing/src/routes/+page.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-	import { Btn } from "@sh/ui/src/lib/components/btn"
-	import Arrow from "@sh/ui/src/lib/icons/Arrow.svelte"
+	import { Input } from "@sh/ui/src/lib/components/input"
 
 	const title = "shorter | world's first domain shortener"
 	const desc = "a domain shortener tool"
@@ -11,3 +10,22 @@
 	<meta content={title} name="title" />
 	<meta content={desc} name="description" />
 </svelte:head>
+
+<div class="pt-28 lg:pt-44">
+	<div class="flex flex-col items-center gap-6 lg:gap-10">
+		<h1
+			class="text-center text-5xl font-thin tracking-[-0.9px] sm:text-6xl lg:text-7xl xl:text-[90px]"
+		>
+			world's first domain <br class="hidden sm:flex" /> shortener
+		</h1>
+		<h2 class="text-center text-primary/70 lg:text-lg">
+			discover a shorter version of your domain, for example
+			<br class="hidden xs:flex" />
+			linktree.com -> linktr.ee
+		</h2>
+		<Input
+			class="h-10 w-full max-w-sm focus-visible:ring-primary/25 lg:h-11"
+			placeholder="type any domain..."
+		/>
+	</div>
+</div>
--- packages/config/tailwind/tailwind.config.cjs
+++ packages/config/tailwind/tailwind.config.cjs
@@ -45,6 +45,9 @@ module.exports = {
 			fontFamily: {
 				sans: ["Manrope", ...fontFamily.sans],
 			},
+			screens: {
+				xs: "480px",
+			},
 		},
 	},
 }
