d4c6a1043e4ad8207fe28b4614316720836fd968
vloe <vloe.dev@gmail.com>
2024-10-21 09:35:59 +0200

    search works

--- apps/server/src/routes/search.rs
+++ apps/server/src/routes/search.rs
@@ -1,13 +1,10 @@
 use crate::{
     constants::tlds::{Tld, TLDS},
     error::AppError,
-    Ctx,
 };
-use axum::{
-    extract::{Query, State},
-    Json,
-};
+use axum::{extract::Query, Json};
 use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 use typeshare::typeshare;
 
 const MAX_DOMAIN: usize = 20;
@@ -57,27 +54,96 @@ pub struct SearchRes {
     domains: Vec<Domain>,
 }
 
-pub async fn mount(
-    State(ctx): State<Ctx>,
-    Query(params): Query<SearchParams>,
-) -> Result<Json<SearchRes>, AppError> {
+pub async fn mount(Query(params): Query<SearchParams>) -> Result<Json<SearchRes>, AppError> {
     params.validate()?;
 
     let sanitized = params.sanitize()?;
-    let (sld, tld, name) = get_domain_parts(&sanitized);
+    let (sld, tld, name) = get_first_domain(&sanitized);
     let mut domains = vec![Domain { name, tld }];
+
+    for perm in domain_perms(&sld) {
+        let parts: Vec<&str> = perm.split('.').collect();
+        let potential_tld = parts[1];
+        if let Some(tld) = TLDS.get(potential_tld) {
+            domains.push(Domain {
+                name: perm,
+                tld: tld.clone(),
+            });
+        }
+    }
 
     let res = SearchRes { domains };
     Ok(Json(res))
 }
 
-fn get_domain_parts(s: &str) -> (String, Tld, String) {
+fn get_first_domain(s: &str) -> (String, Tld, String) {
     let parts: Vec<&str> = s.split('.').collect();
     let sld = parts[0].to_string();
     let tld = parts
         .get(1)
-        .and_then(|&p| TLDS.get(p).cloned())
-        .unwrap_or_else(|| TLDS.get(DEFAULT_TLD).unwrap().clone());
-    let name = format!("{}.{}", sld, &tld.name);
+        .and_then(|&part| TLDS.get(part))
+        .unwrap_or_else(|| TLDS.get(DEFAULT_TLD).unwrap())
+        .clone();
+    let name = format!("{}.{}", sld, tld.name);
     (sld, tld, name)
+}
+
+fn vowel_perms(s: &str) -> Vec<String> {
+    let mut res = vec![s.to_string()];
+
+    let vowels_pos: Vec<usize> = s
+        .chars()
+        .enumerate()
+        .filter(|&(_, char)| "aeiou".contains(char))
+        .map(|(index, _)| index)
+        .collect();
+    let n = vowels_pos.len();
+
+    for i in (0..n).rev() {
+        let mut removed = s.to_string();
+        removed.remove(vowels_pos[i]);
+        res.push(removed.clone());
+        for j in (i + 1..n).rev() {
+            removed.remove(vowels_pos[j] - 1);
+            res.push(removed.clone());
+        }
+    }
+
+    res
+}
+
+fn dot_perms(s: &str) -> Vec<String> {
+    let mut res = Vec::new();
+
+    let chars: Vec<char> = s.chars().collect();
+    let n = chars.len();
+
+    for i in (1..n - 1).rev() {
+        let mut perm = String::new();
+        for (j, &c) in chars.iter().enumerate() {
+            if j == i {
+                perm.push('.');
+            }
+            perm.push(c);
+        }
+        res.push(perm);
+    }
+
+    res
+}
+
+fn domain_perms(s: &str) -> Vec<String> {
+    let mut res = Vec::new();
+    let mut visited = HashSet::new();
+
+    for v in vowel_perms(s) {
+        for d in dot_perms(&v) {
+            if !visited.contains(&d) {
+                visited.insert(d.clone());
+                res.push(d);
+            }
+        }
+    }
+
+    res
 }
