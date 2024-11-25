0974fc478fc5c7d6b1f5b403f7ec543f611137ae
vloe <vloe.dev@gmail.com>
2024-11-25 23:46:30 +0100

    lil fix

--- apps/web/src/routes/404/+page.svelte
+++ apps/web/src/routes/404/+page.svelte
@@ -1,3 +1,12 @@
+<script lang="ts">
+	const title = "404: Page not found"
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+	<meta content={title} name="title" />
+</svelte:head>
+
 <div class="flex h-screen items-center justify-center">
-	<h1 class="text-lg">404: Page not found</h1>
+	<h1 class="text-lg">{title}</h1>
 </div>
