70d180a46d6b945d20d3dad5690dcfd02b88ce2a
vloe <vloe.dev@gmail.com>
2024-12-06 02:12:24 +0100

    fix

--- apps/web/src/lib/utils/urls.ts
+++ apps/web/src/lib/utils/urls.ts
@@ -1,7 +1,7 @@
 import { dev } from "$app/environment"
 
 const API_URL_DEV = "http://localhost:9000"
-const API_URL_PROD = "https://wqf5ycrsaonxgabldcousojjta0yafwm.lambda-url.eu-west-2.on.aws/"
+const API_URL_PROD = "https://ayeqgpklbwm2fj3le6i6ehxkwe0oloez.lambda-url.eu-west-2.on.aws"
 
 export function apiUrl() {
 	const apiUrlStr = dev ? API_URL_DEV : API_URL_PROD
